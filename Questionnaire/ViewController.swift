c4dc6ebc4bf195115b8302918a047bc4799d300a
Marat <Shagiakhmetov2499@gmail.com>
2023-04-10 20:42:10 +0300

    Initial Commit

---
+++ Questionnaire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Questionnaire
+//
+//  Created by Marat Shagiakhmetov on 10.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
